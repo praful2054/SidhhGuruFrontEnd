0b606baa63d2e090f80ac3083cea7716f7d95b46
praful2054 <praful.maheshjha@gmail.com>
2021-05-09 21:43:46 +0530

    commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
 
 const routes: Routes = [
-  {path: '', pathMatch : 'full', redirectTo: 'home'},
-  {path: 'signup', pathMatch : 'full', component: SignupComponent},
-  {path: 'login', pathMatch : 'full', component: LoginComponent},
-  {path: 'home', pathMatch : 'full', component: HomeComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: 'signup', pathMatch: 'full', component: SignupComponent },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
 
- }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
     HeaderComponent,
-    FooterComponent,
-    SignupComponent,
-    LoginComponent
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     IvyCarouselModule,
     MatSliderModule,
     AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     NgbModule
   ],
